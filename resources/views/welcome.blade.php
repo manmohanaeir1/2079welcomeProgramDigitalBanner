403559f4bf6d35e66504b28b0870cf37c68c8dda
ManmohanAeir <manmohanaeir001110@gmail.com>
2022-04-21 20:44:40 +0545

    welcome program banner

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome program</title>
+    <link rel="stylesheet" href="{{url('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('css/slider.css')}}">
+
+    <link rel="stylesheet" href="{{url('css/wcprogram.css')}}">
+    <link rel="stylesheet" href="{{url('css/carousel.css')}}">
+    <link rel="stylesheet" href="{{url('css/candel.css')}}">
+
+
+
+
+</head>
+
+<body>
+    <div class="block">
+        <div class="container">
+            <div class="row  HEADER">
+                <div class="col col-lg-2">
+                    <!-- <img src="{{url('img/logo.jpg')}}" class="img-thumbnail  imgLogo" alt="..."> -->
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner imgLogo">
+                        <div class="carousel-item active">
+                                <img src="{{url('img/tu-logo.png')}}" class="d-block w-100  "  alt="...">
+                            </div>
+                        <div class="carousel-item   ">
+                                <img src="{{url('img/logo.jpg')}}" class="d-block w-100  " style="margin-top: 24px;" alt="...">
+                            </div>
+                           
+                            <!-- <div class="carousel-item">
+                                <img src="{{url('img/flag.png')}}" class="d-block w-100" alt="...">
+                            </div> -->
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-auto">
+                    <h1>SIDDHANATH SCIENCE CAMPUS</h1>
+                    <h3>MAHENDRANAGAR, KANCHANPUR</h3>
+                </div>
+
+                <div class="col-sm">
+                    <img style="max-width:145px;" src="https://mofaga.gov.np/images/np_flag.gif" alt="Local Government Logo">
+
+                </div>
+                <div class="welcome">
+                    <h1>
+                    <!-- <a href="" class="typewrite" data-period="1000" data-type='[ "WELCOME PROGRAM 2079!."'> -->
+                        <a href="" class="typewrite" data-period="2000" data-type='[  "WELCOME TO SIDDHANATH SCIENCE CAMPUS.", "CLASS OF 2079.", "ORGANIZED BY SNSC IT CLUB" ]'>
+                            <span class="wrap"></span>
+                        </a>
+                    </h1>
+                </div>
+
+               
+
+                <div class="row ">
+                    <div class="col-sm">
+                        <!-- <img src="{{url('img/flag.png')}}" class="d-block w-100" alt="..."> -->
+                        <div class="holder">
+                            <div class="candle">
+                                <div class="blinking-glow"></div>
+                                <div class="thread"></div>
+                                <div class="glow"></div>
+                                <div class="flame"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div id="drag-container">
+                            <div id="spin-container">
+                                <!-- Add your images (or video) here -->
+                                <img src="{{url('img/nameste.jpg')}}" alt="">
+                                    <img src="{{url('img/Welcome.jpg')}}" alt="">
+                                <img src="{{url('img/ID/14.png')}}" alt="">
+                                <img src="{{url('img/ID/13.png')}}" alt="">
+                                <img src="{{url('img/ID/4.png')}}" alt="">
+                                <img src="{{url('img/ID/5.png')}}" alt="">
+                                <img src="{{url('img/ID/6.png')}}" alt="">
+                                <img src="{{url('img/ID/7.png')}}" alt="">
+                                <img src="{{url('img/ID/8.png')}}" alt="">
+                                <img src="{{url('img/ID/9.png')}}" alt="">
+                                <img src="{{url('img/ID/10.png')}}" alt="">
+                                <img src="{{url('img/ID/11.png')}}" alt="">
+                                <img src="{{url('img/ID/12.png')}}" alt="">
+                                <img src="{{url('img/ID/15.png')}}" alt="">
+                                <img src="{{url('img/ID/3.png')}}" alt="">
+                                <img src="{{url('img/ID/2.png')}}" alt="">
+                                <img src="{{url('img/ID/1.png')}}" alt="">
+                                <!-- Example image with link -->
+                                <a target="_blank" href="{{url('img/first.jpg')}}" alt="">
+                                    
+
+                                </a>
+
+                                <!-- Example add video  -->
+                                
+                                <!-- Text at center of ground -->
+                                <p>SNSC IT CLUB MEMBERS</p>
+                            </div>
+                            <div id="ground"></div>
+                        </div>
+                       <div class="marquee" style="margin: 50px;">
+                      <marquee behavior="" direction=""> <h2>SPONSORED BY : IT ZONE COMPUTER || SUPPORTED BY: चौलानि पुस्तक भन्डार </h2></marquee>
+                       </div>
+                        <div id="music-container" class="controls loop autoplay ">
+                            <!-- <audio autoplay controls>
+                                <source src="/media/1.mp3" type="audio/mpeg">
+                                <source src="/media/epic.wav" type="audio/wav">
+                            </audio> -->
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm">
+                        <div class="holder" style="float: inline-end;">
+                            <div class="candle">
+                                <div class="blinking-glow"></div>
+                                <div class="thread"></div>
+                                <div class="glow"></div>
+                                <div class="flame"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{url('js/slider.js')}}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="{{url('js/app.js')}}"></script>
+    <script src="{{url('js/bootstarp.min.js')}}"></script>
+    <script src="{{url('js/jquery.js')}}"></script>
+
+
+</body>
+
+</html>
